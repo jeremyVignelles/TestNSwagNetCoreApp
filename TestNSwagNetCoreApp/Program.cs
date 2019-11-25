14ee6cf7443dc08f2784d73eea7c491e46d693f1
Jérémy VIGNELLES <jeremy.vignelles@dev3i.fr>
2019-11-25 10:33:58 +0100

    Updated obsolete method

--- TestNSwagNetCoreApp/Program.cs
+++ TestNSwagNetCoreApp/Program.cs
@@ -29,7 +29,7 @@
                 })
                 .Configure(app =>
                 {
-                    app.UseSwagger()
+                    app.UseOpenApi()
                         .UseSwaggerUi3();
                     app.UseMvc();
                 });
