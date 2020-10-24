92b26f3b761e3bcae49786c93cbe46e0a532a992
Jérémy VIGNELLES <jeremy.vignelles@gmail.com>
2020-10-24 14:12:38 +0200

    Updated code

--- TestNSwagNetCoreApp/Program.cs
+++ TestNSwagNetCoreApp/Program.cs
@@ -6,13 +6,25 @@
     using Microsoft.Extensions.DependencyInjection;
     using NJsonSchema.Generation;
 
+    /// <summary>
+    /// The main class
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// The entry point of the application
+        /// </summary>
+        /// <param name="args">The application arguments</param>
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
         }
 
+        /// <summary>
+        /// The method that creates the builder
+        /// </summary>
+        /// <param name="args">The program arguments</param>
+        /// <returns>The host builder</returns>
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webHost =>
